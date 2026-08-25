80c2b5083b85a9358824ce5ddbf53074f0a5ac2a
Aydan-sharifova <aydanash-apa103@code.edu.az>
2026-08-25 05:03:06 +0400

    Add project fork provenance migration

--- src/Coding.Infrastructure/Migrations/20260825005048_AddProjectForkProvenance.Designer.cs
+++ src/Coding.Infrastructure/Migrations/20260825005048_AddProjectForkProvenance.Designer.cs
@@ -4,6 +4,7 @@ using System.Text.Json;
 using Coding.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Coding.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260825005048_AddProjectForkProvenance")]
+    partial class AddProjectForkProvenance
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Coding.Infrastructure/Migrations/20260825005048_AddProjectForkProvenance.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Coding.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProjectForkProvenance : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ForkedFromProjectId",
+                table: "Projects",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_ForkedFromProjectId",
+                table: "Projects",
+                column: "ForkedFromProjectId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Projects_ForkedFromProjectId",
+                table: "Projects",
+                column: "ForkedFromProjectId",
+                principalTable: "Projects",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Projects_ForkedFromProjectId",
+                table: "Projects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_ForkedFromProjectId",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "ForkedFromProjectId",
+                table: "Projects");
+        }
+    }
+}
